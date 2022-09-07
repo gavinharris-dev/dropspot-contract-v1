0eb443fc6c817853acb95c68caf5f7bee3e9c561
Gavin Harris <gavin.harris@colab.com>
2022-09-07 08:30:53 +0000

    Build 20220907083053.Preprod - Try seeing how long the signatories are

--- src/Plutus/Contracts/DropspotMarket.hs
+++ src/Plutus/Contracts/DropspotMarket.hs
@@ -148,7 +148,8 @@ mkValidator ci mkDatum mkAction context = case mkAction of
   --   2. The Token is being xferd to the buyer
   Buy -> standardBuyConditions   
       -- The Signing Wallet is paid out the NFT
-      &&  Foldable.any (\s -> containsNFT (valuePaidTo txInfo s) (policy mkDatum) (token mkDatum)) (txInfoSignatories txInfo)
+      -- && Foldable.any (\s -> containsNFT (valuePaidTo txInfo s) (policy mkDatum) (token mkDatum)) (txInfoSignatories txInfo)
+      && Foldable.length (txInfoSignatories txInfo) == 1
 
   -- CCBuy -> 
   --         standardBuyConditions 
