42ba895e98a8d0b6fe0da93b9f97d8212edd3d51
Gavin Harris <gavin.harris@colab.com>
2022-09-27 00:29:58 +0000

    Build 20220927002958.Preprod - Testing out Cancel - Cancel has to be signed by the TradeOwner - Testing

--- src/Plutus/Contracts/DropspotMarket.hs
+++ src/Plutus/Contracts/DropspotMarket.hs
@@ -162,8 +162,8 @@ mkValidator ci mkDatum mkAction context = case mkAction of
       &&  outputDatumChangesAreAllowed
 
   Cancel -> -- Trade Owner gets the Token and DS gets its Minting Fee, as long as the Transaction is signed 
-            -- (txSignedBy txInfo (unPaymentPubKeyHash $ tradeOwner mkDatum) || txSignedBy txInfo (unPaymentPubKeyHash $ marketPlaceOwner ci))
-          Value.valueOf (valuePaidTo txInfo (unPaymentPubKeyHash $ tradeOwner mkDatum)) (policy mkDatum) (token mkDatum) >= 1 -- Should we be (in Datum) holding a ref to the number of Tokens that are being 'sold'?
+          txSignedBy txInfo (unPaymentPubKeyHash $ tradeOwner mkDatum)   -- (txSignedBy txInfo (unPaymentPubKeyHash $ tradeOwner mkDatum) || txSignedBy txInfo (unPaymentPubKeyHash $ marketPlaceOwner ci))
+      &&  Value.valueOf (valuePaidTo txInfo (unPaymentPubKeyHash $ tradeOwner mkDatum)) (policy mkDatum) (token mkDatum) >= 1 -- Should we be (in Datum) holding a ref to the number of Tokens that are being 'sold'?
       &&  (Ada.getLovelace (Ada.fromValue (valuePaidTo txInfo (unPaymentPubKeyHash $ marketPlaceOwner ci))) >= DROPSPOT_MINTING_FEE) 
 
   where
